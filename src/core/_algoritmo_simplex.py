f768c8104a8c589cbc3316565224f4526d7d4a0a
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-01-10 11:13:20 -0500

    Resolucao para problemas LP e FLP finalizada
    Consegue resolver pelo metodo .simplex tanto o problema LP como o problema FLP. Apaguei os arquivos base nos quais criei inicialmente tudo (arquivos .ipynb) O seguinte passo consiste testar varios problemas LP para validar o funcionamento e corrijir possiveis falhas. Com isso feito, procede-se a deixar tudo padronizado, facil de entender, para no fim criar doc introdutorio e depois fazê-lo tudo publico no GitHub.

--- src/core/_algoritmo_simplex.py
+++ src/core/_algoritmo_simplex.py
@@ -37,7 +37,7 @@ def funcao_calculo_iteracoes(lista_todas_variaveis, coeficiente_base_tableau_C_j
 ### Funcao valores C_j e vetor C_B
         # 1 Define-se os valores de todas as variaveis base do tableau C_j (consideram-se todas as variaveis) 
 def funcao_C_j_e_C_B(lista_todas_variaveis, lista_fo):
-    M = 10
+    M = 10 
     coeficiente_base_tableau_C_j = []
     for i, variavel in enumerate(lista_todas_variaveis):
         if 'x' in variavel:
@@ -65,28 +65,46 @@ def funcao_C_j_e_C_B(lista_todas_variaveis, lista_fo):
 
 ### Funcao loop (while) até C_j menos Z_j ser <=0
 def funcao_loop_C_jmenosZ_j_ate_menor_a_0(lista_matriz_A, lista_todas_variaveis, vetor_variaveis_C_B, vetor_coeficientes_C_B, coeficiente_base_tableau_C_j, vetor_C_j_menos_Z_j, lista_coef_b):
+    lista_C_j_Z_j_evitar_infinito = []
+    k=0
     while any(e > 0 for e in vetor_C_j_menos_Z_j):
         print('-------------------------------------------------------------------------------------')
-        print('# Inicia loop')
+        print(f'# Inicia loop n°{k+1}')
         # Caso se calcula a base que entra-sai, entao comeca-se por definir o maior valor de C_j-Z_j, e define-se a coluna a utilizar (.index()) e a variavel
-        maior_valor_C_j_Z_j = max(vetor_C_j_menos_Z_j)
+        # ALTERACAO PARA EVITAR LOOP INFINITO - TESTE
+        print('vetor_C_j_menos_Z_j', vetor_C_j_menos_Z_j)
+        lista_C_j_Z_j_evitar_infinito.append(tuple(vetor_C_j_menos_Z_j))
+        if len(set(lista_C_j_Z_j_evitar_infinito)) == len(lista_C_j_Z_j_evitar_infinito):
+            maior_valor_C_j_Z_j = max(vetor_C_j_menos_Z_j)
+        else:
+            print('Parece que entramos em loop infinito, vamos corrijir para C_j-Z_j!')
+            vetor_C_j_menos_Z_j[vetor_C_j_menos_Z_j.index(max(vetor_C_j_menos_Z_j))] = vetor_C_j_menos_Z_j[vetor_C_j_menos_Z_j.index(max(vetor_C_j_menos_Z_j))] - 1
+            maior_valor_C_j_Z_j = max(vetor_C_j_menos_Z_j)
+        
         indice_maior_valor_C_j_Z_j = vetor_C_j_menos_Z_j.index(maior_valor_C_j_Z_j) # INDICE COLUNA PIVO
+        # print('indice_maior_valor_C_j_Z_j', indice_maior_valor_C_j_Z_j)
         coluna_pivo = lista_todas_variaveis[indice_maior_valor_C_j_Z_j]
+
         # Depois, se calcula a razao entre a coluna pivo e o seu correspondente b (vetor_b ou lista_coef_b)
             # Caso o valor da razao sea positivo, eh considerado no vetor razao
         vetor_razao = []
         for i, dividendo in enumerate(lista_coef_b):
             divisor = lista_matriz_A[i][indice_maior_valor_C_j_Z_j]
-            razao = dividendo / divisor
-            if razao >= 0:
-                vetor_razao.append(razao)
-            else:
+            if divisor == 0:
                 vetor_razao.append(0)
+            else:
+                razao = dividendo / divisor
+                if razao >= 0:
+                    vetor_razao.append(razao)
+                else:
+                    vetor_razao.append(0)
             # O valor minimo do vetor razao vai definir qual elemento sai da variaveis base, e qual elemento entra 
         vetor_razao_sem0 = vetor_razao[:]
-        if 0 in vetor_razao_sem0:
-            vetor_razao_sem0.remove(0)
+        vetor_razao_sem0 = list(filter(lambda x: x != 0, vetor_razao))
+        
+        print('vetor_razao', vetor_razao)
         menor_valor_razao = min(vetor_razao_sem0)    
+        print('menor_valor_razao:',menor_valor_razao)
         indice_menor_valor_razao = vetor_razao.index(menor_valor_razao)
         fila_pivo =  vetor_variaveis_C_B[indice_menor_valor_razao] 
             # Se troca a variavel que sai pela que entra
@@ -99,6 +117,7 @@ def funcao_loop_C_jmenosZ_j_ate_menor_a_0(lista_matriz_A, lista_todas_variaveis,
             indice_a_ser_considerado = lista_todas_variaveis.index(variavel_C_B)
             valor_a_ser_substituido_em_C_B = coeficiente_base_tableau_C_j[indice_a_ser_considerado]       
             vetor_coeficientes_C_B[i] = valor_a_ser_substituido_em_C_B
+
         print('novo vetor variaveis C_B:', vetor_variaveis_C_B)
         print('novo vetor C_B:', vetor_coeficientes_C_B)
         print('matriz_A:', lista_matriz_A)
@@ -113,13 +132,19 @@ def funcao_loop_C_jmenosZ_j_ate_menor_a_0(lista_matriz_A, lista_todas_variaveis,
             lista_coef_b,
             coluna_pivo
             )
+
         print('matriz A tratada:', lista_matriz_A)
         print('vetor_b tratado', lista_coef_b)    
         print('\n     # Inicia calculo Z_j_e_Z_j_menos_C_j')
         vetor_Z_j, vetor_C_j_menos_Z_j = funcao_calculo_Z_j_e_Z_j_menos_C_j(lista_todas_variaveis, lista_matriz_A, coeficiente_base_tableau_C_j, vetor_coeficientes_C_B)
-        print('vetor_Z_j', vetor_Z_j) 
+
+        # print('vetor_Z_j', vetor_Z_j) 
         print('vetor_C_j_menos_Z_j', vetor_C_j_menos_Z_j)
+        if k==10:
+            break
+        k+=1
     otimo_fo = sum(elemento_C_B*lista_coef_b[i] for i, elemento_C_B in enumerate(vetor_coeficientes_C_B) )
+
     texto_fim_loop = f'''-------------------------------------------------------
     -------------------------------------------------------
     Fim do loop:
@@ -127,4 +152,5 @@ def funcao_loop_C_jmenosZ_j_ate_menor_a_0(lista_matriz_A, lista_todas_variaveis,
     valores f.o. fase1: {coeficiente_base_tableau_C_j}
     matriz_A: {lista_matriz_A}
     vetor_b: {lista_coef_b}'''   
+    [print(lista) for lista in lista_C_j_Z_j_evitar_infinito]
     return  print(texto_fim_loop)
--- src/otimizador.py
+++ src/otimizador.py
@@ -54,7 +54,7 @@ class Otimizador:
         print(
             'VARIAVEIS:', lista_todas_variaveis,
             '\nVETOR FUNÇÃO OBJETIVO:', lista_fo,
-            '\nMATRIZ A:' , [ linha for linha in lista_matriz_A],
+            '\nMATRIZ A:\n' + '\n'.join( '\t'+str(linha) for linha in lista_matriz_A),
             '\nVETOR b:', lista_coef_b
             )
     
@@ -101,20 +101,21 @@ class Otimizador:
 
 # Programa
 otimizacao_emilio = Otimizador()
-otimizacao_emilio.mostrar_funcao_objetivo()
-otimizacao_emilio.mostrar_restricoes()
-otimizacao_emilio.simplex()
+# otimizacao_emilio.mostrar_funcao_objetivo()
+# otimizacao_emilio.mostrar_restricoes()
+# otimizacao_emilio.simplex()
 
-otimizacao_emilio.adicionar_funcao_objetivo('max. + 0x_1 +  0x_3 + 0x_2 + 0x_4 + 1x_5')
-otimizacao_emilio.adicionar_restricao('+ 3000x_1  + 30000x_3 + 20000x_2 + 10000x_4 + 37777x_5 <= 137777')
-otimizacao_emilio.adicionar_restricao('+ 1x_1 +  1x_3 + 1x_2 + 1x_4 + 5x_5 == 25')
-otimizacao_emilio.adicionar_restricao('+ 20x_1 +  10x_3 + 5x_2 + 2x_4 + 250x_5 <= 500')
-otimizacao_emilio.adicionar_restricao('+ 10x_1  + 20x_3 + 20x_2 +  15x_4 - 50x_5 >= 50')
-otimizacao_emilio.adicionar_restricao('+ 0x_1  + 0x_3 + 0x_2 + 0x_4 + 1x_5 <= 1')
+# otimizacao_emilio.adicionar_funcao_objetivo('max. + 0x_1 +  0x_3 + 0x_2 + 0x_4 + 1x_5')
+# otimizacao_emilio.adicionar_restricao('+ 3000x_1  + 30000x_3 + 20000x_2 + 10000x_4 + 37777x_5 <= 137777')
+# otimizacao_emilio.adicionar_restricao('+ 1x_1 +  1x_3 + 1x_2 + 1x_4 + 5x_5 == 25')
+# otimizacao_emilio.adicionar_restricao('+ 20x_1 +  10x_3 + 5x_2 + 2x_4 + 250x_5 <= 500')
+# otimizacao_emilio.adicionar_restricao('+ 10x_1  + 20x_3 + 20x_2 +  15x_4 - 50x_5 >= 50')
+# otimizacao_emilio.adicionar_restricao('+ 0x_1  + 0x_3 + 0x_2 + 0x_4 + 1x_5 <= 1')
 # otimizacao_emilio.adicionar_restricao('+ 0x_1  + 0x_3  + 0x_2 + 0x_4 + 1x_5 >= 0')
 
 # otimizacao_emilio.mostrar_funcao_objetivo()
 # otimizacao_emilio.mostrar_restricoes()
+# otimizacao_emilio.mostrar_problema()
 
-# otimizacao_emilio.simplex()
+otimizacao_emilio.simplex()
 
