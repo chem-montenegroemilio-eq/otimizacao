fca21bbc4194c4eb67feffdd6623baf7cc7adb29
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-01-28 22:14:47 -0500

    test: Testei o problema do Gut com .mostrar_problema() e deu certo

--- src/otimizador.py
+++ src/otimizador.py
@@ -201,6 +201,7 @@ otimizacao_emilio.adicionar_restricao('+ 1x_1 + 1x_2 <= 10')
 otimizacao_emilio.adicionar_restricao('- 1x_1 + 0x_2 <= 0')
 otimizacao_emilio.adicionar_restricao('+ 0x_1 - 1x_2 <= 0')
 otimizacao_emilio.mostrar_problema()
+otimizacao_emilio.simplex()
 
 # SOLUCIONAR PROBLEMA SIMPLEX COM RESULTADOS LP OBTIDOS PARA FASE 2
 # otimizacao_emilio.adicionar_funcao_objetivo('min. + 3000x_1 + 20000x_2 + 30000x_3 + 10000x_4 + 0x_5 + 0x_6')
