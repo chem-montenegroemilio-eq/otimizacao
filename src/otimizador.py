2d27bc5f45d929b793c7af660ed86468633d1e2b
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-01-28 22:05:02 -0500

    test: Testei problema do Gut e deu erro
    O problema do livro do gut possui dois coeficientes com decimais (-0.062 e -0.074)na sua funcao objetivo, pelo qual na hora da funcao _tratamento_dados() querer separar o min. ou max, acaba criando uma lista com muitos elementos, e so precisa de dois

--- src/otimizador.py
+++ src/otimizador.py
@@ -193,10 +193,19 @@ otimizacao_emilio = Otimizador()
 # otimizacao_emilio.mostrar_problema()
 # otimizacao_emilio.simplex()
 
+# TESTE DO PROBLEMA DO GUT
+otimizacao_emilio.adicionar_funcao_objetivo('min. - 0.062x_1 - 0.074x_2')
+otimizacao_emilio.adicionar_restricao('+ 1x_1 + 1x_2 <= 10')
+otimizacao_emilio.adicionar_restricao('- 1x_1 + 0x_2 <= 0')
+otimizacao_emilio.adicionar_restricao('+ 0x_1 - 1x_2 <= 0')
+otimizacao_emilio.mostrar_problema()
+
 # SOLUCIONAR PROBLEMA SIMPLEX COM RESULTADOS LP OBTIDOS PARA FASE 2
-otimizacao_emilio.adicionar_funcao_objetivo('min. + 3000x_1 + 20000x_2 + 30000x_3 + 10000x_4 + 0x_5 + 0x_6')
-otimizacao_emilio.adicionar_restricao('+ 10x_1 + 0x_2 + 0x_3 + 5x_4 + 0x_5 + 1x_6 <= 320')
-otimizacao_emilio.adicionar_restricao('+ 10x_1 - 5x_2 + 0x_3 - 8x_4 + 1x_5 + 0x_6 <= 0')
-otimizacao_emilio.adicionar_restricao('+ 1x_1 + 1x_2 + 1x_3 + 1x_4 + 0x_5 + 0x_6 <= 20')
+# otimizacao_emilio.adicionar_funcao_objetivo('min. + 3000x_1 + 20000x_2 + 30000x_3 + 10000x_4 + 0x_5 + 0x_6')
+# otimizacao_emilio.adicionar_restricao('+ 10x_1 + 0x_2 + 0x_3 + 5x_4 + 0x_5 + 1x_6 <= 320')
+# otimizacao_emilio.adicionar_restricao('+ 10x_1 - 5x_2 + 0x_3 - 8x_4 + 1x_5 + 0x_6 <= 0')
+# otimizacao_emilio.adicionar_restricao('+ 1x_1 + 1x_2 + 1x_3 + 1x_4 + 0x_5 + 0x_6 <= 20')
 # otimizacao_emilio.mostrar_problema()
-otimizacao_emilio.simplex()
+# otimizacao_emilio.simplex()
+
+ 
