a8e31303c87e739b8de67a5d1e93675c59cd2aaa
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-04-01 08:07:46 -0500

    refactor: Facilitei visualizacao dos exemplos chem e compras

--- exemplos/exemplo_compras.py
+++ exemplos/exemplo_compras.py
@@ -1,17 +1,16 @@
 # from otimizacao import Otimizador
 from otimizacao import Otimizador
 
-# print(otimizacao.__file__)
-
 # SOLUCIONAR PROBLEMA SIMPLEX COM RESULTADOS LP 
 otimizacao_emilio = Otimizador()
 otimizacao_emilio.adicionar_funcao_objetivo('min. + 3000x_1 + 20000x_2 + 30000x_3 + 10000x_4 ')
 otimizacao_emilio.adicionar_restricao('+ 1x_1 + 1x_2 + 1x_3 + 1x_4  == 20')
 otimizacao_emilio.adicionar_restricao('+ 20x_1 + 5x_2 + 10x_3 + 2x_4 <= 200')
 otimizacao_emilio.adicionar_restricao('+ 10x_1 + 20x_2 + 20x_3 + 15x_4 >= 80')
-# otimizacao_emilio.mostrar_problema()
 otimizacao_emilio.simplex(calculo_visivel=False)
 
+
+# VERSAO FLP
 # otimizacao_emilio.adicionar_funcao_objetivo('max. + 0x_1 +  0x_3 + 0x_2 + 0x_4 + 1x_5')
 # otimizacao_emilio.adicionar_restricao('+ 3000x_1  + 30000x_3 + 20000x_2 + 10000x_4 + 37777x_5 <= 137777')
 # otimizacao_emilio.adicionar_restricao('+ 1x_1 +  1x_3 + 1x_2 + 1x_4 + 5x_5 == 25')
