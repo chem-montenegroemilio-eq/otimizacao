4b3ee53f585248691aa8fb546d6ee682f1adc4cc
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-04-01 08:17:10 -0500

    fix: Corrigir o range de ate 20 do determinador_fase
    Coloquei um enumerate para que percorra a lista das variaveis ate o que de o enumerate (entao nao eh 20, vai ser a magnitude do enumerate da lista)

--- src/otimizacao/core/determinador_fase.py
+++ src/otimizacao/core/determinador_fase.py
@@ -1,7 +1,7 @@
 def funcao_caso_haja_variaveis_artificias(lista_todas_variaveis):
     variaveis_artificiais = []
     teste_a = 'a_'
-    for i in range(1, 20):
+    for i, variavel in enumerate(lista_todas_variaveis):
         variacao_teste_a = f'{teste_a}{i}'
         if variacao_teste_a in lista_todas_variaveis:
             variaveis_artificiais.append(variacao_teste_a)
