181133057fbb9bd97e954fa179743180f0b4140d
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-04-04 16:22:30 -0500

    teste: A leitura das equacoes deve ser mais flexivel, para ler maior complexidade.
    Precisa aprimorar-se o parser.py: ler f.o. que nao tenha '+' ou '-' no inicio, ou que nao tenha coeficiente (este podendo ser 0). Para depois, tambem salvar restricao que nao tenha variavel (coeficiente=0)

--- exemplos/exemplo_chem_eng.py
+++ exemplos/exemplo_chem_eng.py
@@ -1,3 +1,14 @@
 # AINDA NAO ESSTA PRONTO
 
 # SOLUCIONAR PROBLEMA DO HIMMELBLAU 
+from otimizacao import Otimizador as otm
+# Exemplo de otimizacao LP do Himmelblau
+fo = 'min.  + 1x_1 + 6x_2 - 7x_3 + 1x_4 + 5x_5'
+restricao1 = '+ 5x_1 - 4x_2 + 13x_3 - 2x_4 + 1x_5 == 20'
+restricao2 = '+ 1x_1 - 1x_2 + 5x_3 - 1x_4 + 1x_5 == 8'
+
+teste = otm() 
+teste.adicionar_funcao_objetivo(funcao_objetivo= fo)
+teste.adicionar_restricao(restricao1)
+teste.adicionar_restricao(restricao2)
+teste.simplex(calculo_visivel=True)
