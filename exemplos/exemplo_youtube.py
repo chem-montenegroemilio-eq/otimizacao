e890d02b887263ab830612490365b6c9bfde049f
chem-montenegroemilio-eq <chem.montenegro.eq@gmail.com>
2026-04-04 10:43:23 -0500

    chore: prepara repositorio para publicacao v0.1.0

--- exemplos/exemplo_youtube.py
+++ exemplos/exemplo_youtube.py
@@ -1,12 +1,10 @@
 from otimizacao import Otimizador
 
-otimizacao_emilio = Otimizador()
+otimizacao_youtube = Otimizador()
 # EXEMPLO YOUTUBE: https://www.youtube.com/watch?v=btjxqq-vMOg&t=300s
-otimizacao_emilio.adicionar_funcao_objetivo('max. + 6x_1 - 7x_2 - 4x_3')
-otimizacao_emilio.adicionar_restricao('+ 2x_1 + 5x_2 - 1x_3 <= 18')
-otimizacao_emilio.adicionar_restricao('- 1x_1 + 1x_2 + 2x_3 >= 14')
-otimizacao_emilio.adicionar_restricao('+ 3x_1 +  2x_2 + 2x_3 == 26')
+otimizacao_youtube.adicionar_funcao_objetivo('max. + 6x_1 - 7x_2 - 4x_3')
+otimizacao_youtube.adicionar_restricao('+ 2x_1 + 5x_2 - 1x_3 <= 18')
+otimizacao_youtube.adicionar_restricao('- 1x_1 + 1x_2 + 2x_3 >= 14')
+otimizacao_youtube.adicionar_restricao('+ 3x_1 +  2x_2 + 2x_3 == 26')
 # otimizacao_emilio.mostrar_problema()
-teste = otimizacao_emilio.simplex(calculo_visivel=True)
-
-# print(teste)
+teste = otimizacao_youtube.simplex(calculo_visivel=True)
